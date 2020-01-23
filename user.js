76ac3f4512c89572c4208fc7ba78fe25423c2421
Ben Tagtow <bentagtow@gmail.com>
2020-01-23 14:40:56 -0700

    ability to push to backend

--- user.js
+++ user.js
@@ -1,19 +1,29 @@
 const response = (response => response.json())
 const weekURL = 'http://localhost:3000/weeks'
+const selectionURL = 'http://localhost:3000/selections'
 const gameURL = 'http://localhost:3000/games'
 const searchParams = new URLSearchParams(window.location.search)
 const weekDropDown = document.getElementById('week-drop-down')
 const weekContainer = document.getElementById('week-container')
+const week1Container = document.getElementById('week1-container')
+const week2Container = document.getElementById('week2-container')
+const week3Container = document.getElementById('week3-container')
+const week4Container = document.getElementById('week4-container')
+const week5Container = document.getElementById('week5-container')
 const gameContainer = document.getElementById('game-container')
+const pointsIndicator = document.getElementById('points')
+const welcomeContainer = document.getElementById('welcome-container')
+const profilePic = document.getElementById('image')
+const selectionContainer = document.getElementById('selection-container')
+gameContainer.append(week1Container,week2Container,week3Container,week4Container,week5Container)
 const welcomeUser = document.getElementById('welcome-message')
 const gameWeekArray = []
-let back = document.getElementById('back')
+const back = document.getElementById('back')
 const id = searchParams.get('id')
 const body = document.body
 
 document.addEventListener("DOMContentLoaded", () => {
     welcomeMessage()
-    homeButton()
     // fetchGameWeeks()
     // weekSelector()
     // gameSelector()
@@ -24,18 +34,24 @@ function welcomeMessage(){
     .then(response)
     .then(user => {
         let welcome = document.createElement('h1')
+        let points = document.createElement('h5')
+        let backButton = document.createElement('button')
+        let image = document.createElement('img')
+        
+        
         welcome.innerText = `${user.first} ${user.last}`
+        points.innerText = `Points: ${user.points}`
+        backButton.innerHTML=`<a href="http://localhost:3001/">Select new user</a>`
+        image.src = user.image
+
         welcomeUser.append(welcome)
+        pointsIndicator.append(points)
+        back.append(backButton)
+        profilePic.append(image)
     }
     )
 }
 
-function homeButton(){
-    let backButton =document.createElement('button')
-    backButton.innerHTML=`<a href="http://localhost:3001/">Select new user</a>`
-    back.append(backButton)
-
-}
 
 function weekSelector(){
     fetch(`http://localhost:3000/weeks`)
@@ -68,98 +84,85 @@ function weekListing(games){
 }
 function appendAllGames(games){
     games.map(game => {
-    let individualGameContainer = document.createElement('div')
-    let gameListing = document.createElement('p')
-    let homeTeamButton = document.createElement('button')
-    let awayTeamButton = document.createElement('button')
-    homeTeamButton.innerText = "select home"
-    awayTeamButton.innerText = "select away"
-    gameListing.id = game.id
-    gameListing.innerHTML = `<a>Week ${game.week.week} - Home: ${game.home.name} vs. Away: ${game.away.name}</a>`
-    gameContainer.append(individualGameContainer)
-    individualGameContainer.append(gameListing)
-    gameListing.append(homeTeamButton)
-    gameListing.append(awayTeamButton)
+        return game.week.week==1 ? addGamesAndButtons(game, week1Container)
+        : game.week.week==2 ? addGamesAndButtons(game, week2Container)
+        : game.week.week==3 ? addGamesAndButtons(game, week3Container)
+        : game.week.week==4 ? addGamesAndButtons(game, week4Container)
+        : game.week.week==5 ? addGamesAndButtons(game, week5Container)
+        : console.log("done")
+        // addGamesAndButtons(week2Container)
+        // addGamesAndButtons(week3Container)
+        // addGamesAndButtons(week4Container)
+        // addGamesAndButtons(week5Container)
 
-    // homeTeamButton.addEventListener('click', () => {
-    //     event.preventDefault()
-    //     let choice = game.home
-        
+    
 
-        
-    // })
-
-}
-)
-}
-// createUser.addEventListener('submit', () => {
-//     event.preventDefault()
-//     let formData = new FormData(createUser)
-//     let firstName = formData.get('first-name')
-//     let lastName = formData.get('last-name')
-//     let image = formData.get('image')
-
-//     body = {
-//         'first': firstName,
-//         'last': lastName,
-//         'points': "2",
-//         'status': 'alive',
-
-//         image
-//         // team
-//     }
-//     fetch(userURL, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(body)
-
-//     })
-//     window.alert(`Hi ${firstName}, your profile has been created.`)
-
-
-
-function pushSelections(button,homeOrAway){
-
-    button.addEventListener('click', () => {
-        
+          
     })
 
 }
 
-// function()
+function addGamesAndButtons(game, specificWeekContainer){
+    let individualGameContainer = document.createElement('div')
+    let gameListing = document.createElement('p')
+    let homeTeamButton = document.createElement('button')
+    let awayTeamButton = document.createElement('button')
+    homeTeamButton.innerText = `select ${game.home.name}`
+    awayTeamButton.innerText = `select ${game.away.name}`
+    gameListing.id = game.id
+    gameListing.innerHTML = `<a>Week ${game.week.week}: Home: ${game.home.name} vs. Away: ${game.away.name}</a>`
+    specificWeekContainer.append(individualGameContainer)
+    individualGameContainer.append(gameListing)
+    individualGameContainer.append(homeTeamButton)
+    individualGameContainer.append(awayTeamButton)
 
-    // const unique = game
-    
-    
-    // weekDropDown.addEventListener("change", () => {
-        //     let weekId = weekDropDown.options[weekDropDown.selectedIndex].id
-        //     fetch(weekURL)
-        //         .then(response)
-        //         .then(weeks => {
-            //             const selectedWeek = (weeks.find(week => {
-                //                 week.id == weekId
-                //             }
-                //             )
-                //             ) 
-                //         function fetchGameWeeks(){
-                    //             fetch(gameURL)
-                    //                 .then(response)
-                    //                 .then(games => games.map(game => {
-                        //                     const gameWeek = (game.week)
-                        //                     if (gameWeek.id == selectedWeek){
-                            //                         console.log(game) 
-                            //                     }
-                            //                 }))
-                            //             }
-                            //         fetchGameWeeks()
-                            //         }
-                            //     )
-                            // }
-                            // )
-                            
-                            
+    homeTeamButton.addEventListener('click', () => {
+        event.preventDefault()
+        let choice = game.home.name
+        let week_id = game.week.id
+        let body = {
+            'winner':choice,
+            week_id
+        }
+        fetch(selectionURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify(body)
+        }
+        )
+        let selection = document.createElement('li')    
+        selection.innerText = `Week ${game.week.week} selection: ${game.home.name}`
+        selectionContainer.append(selection)
+    })
+    awayTeamButton.addEventListener('click', () => {
+        event.preventDefault()
+        let choice = game.away.name
+        let week_id = game.week.id
+        let body = {
+            'winner':choice,
+            week_id
+        }
+        fetch(selectionURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify(body)
+        }
+        )    
+        let selection = document.createElement('li')    
+        selection.innerText = `Week ${game.week.week} selection: ${game.away.name}`
+        selectionContainer.append(selection)
+    })
+} 
+
+function removeGamesAndButtons(){
+
+}
+
+
 function onlyUnique(value, index, self) { 
     return self.indexOf(value) === index;
 }
